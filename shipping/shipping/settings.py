c045683a4787c9faaf2c0b6922f77a639b656eed
Marco Lavagnino <marcolavagnino@gmail.com>
2019-02-14 10:47:22 -0300

    add geodjango

--- shipping/shipping/settings.py
+++ shipping/shipping/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/1.9/ref/settings/
 """
 
 import os
+import json
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -37,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.gis',
 ]
 
 MIDDLEWARE_CLASSES = [
@@ -74,11 +76,18 @@ WSGI_APPLICATION = 'shipping.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.9/ref/settings/#databases
 
+DEVELOPMENT_DB = {
+    'ENGINE': 'django.contrib.gis.db.backends.spatialite',
+    'NAME': os.path.join(BASE_DIR, 'db.spatialite'),
+}
+
+if os.getenv('DATABASE'):
+    DATABASE = json.loads(os.getenv('DATABASE'))
+else:
+    DATABASE = DEVELOPMENT_DB
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+    'default': DATABASE
 }
 
 
